28d92bba44dda255d23e4e71f20aaa4eecf668cd
Руслан <kraktus@mail.ru>
2014-04-09 03:09:03 +0400

    Верстка списка медиа-публикаций

--- wp-content/themes/yoo_capture_wp/warp/systems/wordpress/layouts/_post-media.php
+++ wp-content/themes/yoo_capture_wp/warp/systems/wordpress/layouts/_post-media.php
@@ -10,22 +10,68 @@
 
     <h1 class="uk-article-title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
-    <p class="uk-article-meta">
-        <?php
-            $date = '<time datetime="'.get_the_date('Y-m-d').'" pubdate>'.get_the_date().'</time>';
-            printf(__('Written by %s on %s. Posted in %s', 'warp'), '<a href="'.get_author_posts_url(get_the_author_meta('ID')).'" title="'.get_the_author().'">'.get_the_author().'</a>', $date, get_the_category_list(', '));
-        ?>
-    </p>
 
+
+
+   <br>
+                <a href="">
+                    <div  class="nradio_recommend" style="position: relative;float: right;margin-top: -80px;"></div>
+                </a>
+   <table border="0" cellpadding="1" cellspacing="1" style="width: 100%;">
+	<tbody>
+		<tr>
+			<td rowspan="3" style="width: 250px;">
+                 <a class="uk-thumbnail uk-overlay-toggle" style="padding: 0px;" href="<?php the_permalink() ?>">
+                    <div class="uk-overlay" style="border: 3px solid #7d2422;">
+                        <img src="http://www.foolsday.ru/wp-content/uploads/2014/03/4cUoGC_jR3c.jpg" style="max-height:473px;" alt="Load Style" >
+                        <div class="uk-overlay-area"></div>
+                    </div>
+                 </a>
+			</td>
+			<td style="width: 5%">&nbsp;</td>
+			<td style="vertical-align: top; width: 169px;">
+                <hr>
+                Conversion rate синхронизирует потребительский рынок, отвоевывая рыночный сегмент. В рамках концепции Акоффа и Стэка, партисипативное планирование тормозит бюджет на размещение, полагаясь на инсайдерскую информацию.
+                   <br><hr>
+			</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+			<td style="text-align: right; vertical-align: bottom;">
+                <div style="zoom: 3; color: #7C2322">
+                    <div class="uk-icon-camera" style="float: left;"></div>
+                    <div class="uk-icon-youtube-play" style="float: left; margin-left: 10px;"></div>
+                </div>
+                    <a class="uk-button uk-button-primary " href="<?php the_permalink() ?>">Подробнее</a>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+
+
+<!--    <p class="uk-article-meta">
+        <?php
+/*            $date = '<time datetime="'.get_the_date('Y-m-d').'" pubdate>'.get_the_date().'</time>';
+            printf(__('Written by %s on %s. Posted in %s', 'warp'), '<a href="'.get_author_posts_url(get_the_author_meta('ID')).'" title="'.get_the_author().'">'.get_the_author().'</a>', $date, get_the_category_list(', '));
+        */?>
+    </p>
+-->
     <?php the_content(''); ?>
 
-    <ul class="uk-subnav uk-subnav-line">
-        <li><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php _e('Continue Reading', 'warp'); ?></a></li>
-        <?php if(comments_open()) : ?>
-            <li><?php comments_popup_link(__('No Comments', 'warp'), __('1 Comment', 'warp'), __('% Comments', 'warp'), "", ""); ?></li>
-        <?php endif; ?>
-    </ul>
-
+<!--    <ul class="uk-subnav uk-subnav-line">-->
+<!--        <li><a href="--><?php //the_permalink() ?><!--" title="--><?php //the_title_attribute(); ?><!--">--><?php //_e('Continue Reading', 'warp'); ?><!--</a></li>-->
+<!--        --><?php //if(comments_open()) : ?>
+<!--            <li>--><?php //comments_popup_link(__('No Comments', 'warp'), __('1 Comment', 'warp'), __('% Comments', 'warp'), "", ""); ?><!--</li>-->
+<!--        --><?php //endif; ?>
+<!--    </ul>-->
+    <br>
     <?php edit_post_link(__('Edit this post.', 'warp'), '<p><i class="uk-icon-pencil"></i> ','</p>'); ?>
 
 </article>
+<hr>
+<br>
