3e800e3f3380625496e303d4ebcbe65148c8405b
Руслан <kraktus@mail.ru>
2014-04-28 13:52:18 +0400

    Корректное отображение даты на русском языке

--- wp-content/themes/yoo_capture_wp/functions.php
+++ wp-content/themes/yoo_capture_wp/functions.php
@@ -15,6 +15,64 @@ function fb_change_search_url_rewrite() {
 }
 add_action( 'template_redirect', 'fb_change_search_url_rewrite' );
 
+
+
+/*Функция правильного русскоязычного отображения даты и времени*/
+function rdate($format, $timestamp, $case = 0)
+{
+    if ( $timestamp === null )
+        $timestamp = time();
+
+    static $loc =
+    'Январ,ь,я,е,ю,ём,е
+    Феврал,ь,я,е,ю,ём,е
+    Март, ,а,е,у,ом,е
+    Апрел,ь,я,е,ю,ем,е
+    Ма,й,я,е,ю,ем,е
+    Июн,ь,я,е,ю,ем,е
+    Июл,ь,я,е,ю,ем,е
+    Август, ,а,е,у,ом,е
+    Сентябр,ь,я,е,ю,ём,е
+    Октябр,ь,я,е,ю,ём,е
+    Ноябр,ь,я,е,ю,ём,е
+    Декабр,ь,я,е,ю,ём,е';
+
+    if ( is_string($loc) )
+    {
+        $months = array_map('trim', explode("\n", $loc));
+        $loc    = array();
+
+        foreach($months as $monthLocale)
+        {
+            $cases = explode(',', $monthLocale);
+            $base  = array_shift($cases);
+
+            $cases = array_map('trim', $cases);
+
+            $loc[] = array(
+                'base'  => $base,
+                'cases' => $cases,
+            );
+        }
+    }
+
+    $m = (int)date('n', $timestamp)-1;
+
+    $F = $loc[$m]['base'].$loc[$m]['cases'][$case];
+
+    $format = strtr($format, array(
+        'F' => $F,
+        'M' => substr($F, 0, 3),
+    ));
+
+    return date($format, $timestamp);
+}
+
+
+
+
+
+
 // check compatibility
 if (version_compare(PHP_VERSION, '5.3', '>=')) {
 
