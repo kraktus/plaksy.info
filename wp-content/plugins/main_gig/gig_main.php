7da2a498fa96e8c01055386be71013cf1fc3839e
Руслан <kraktus@mail.ru>
2014-04-30 16:19:45 +0400

    Видежет концерты для главной странцы Верстка натянута на движ.
    Не работает api контакта

--- wp-content/plugins/main_gig/gig_main.php
+++ wp-content/plugins/main_gig/gig_main.php
@@ -7,23 +7,83 @@ Author: Руслан Жамалетдинов
 Author URI: vk.com/krakuts
 */
 
-function gig_main_widget($args) {
+function gig_main_widget($args)
+{
     extract($args, EXTR_SKIP);
     $options = get_option('gig_main_widget');
-    $title = empty($options['title']) ? 'Gig Main' : $options['title'];
-?>
-        <!-- <?php echo $before_widget; ?> -->
-        <!-- <?php echo $before_title . $title . $after_title; ?> -->
+    $title   = empty($options['title']) ? 'Gig Main' : $options['title'];
+
+    /*Делаем выборку из всех.*/
+
+    /*get_posts - добавляем цикл*/
+    /*query_posts - изменяем базовый*/
+
+    /*запрос на след концерт*/
+    $posts = get_posts(
+        array(
+            'posts_per_page' => 1,
+            'post_type'      => 'concert',
+            'order'          => 'ASC',
+            'meta_key'       => 'wpcf-date',
+            'orderby'        => 'wpcf-date',
+            'meta_query'     => array(
+                array('key'     => 'wpcf-date',
+                      'value'   => time(), //Время концерта больше текущей даты
+                      'compare' => '>'
+                )
+            )
+        )
+    );
+    /*перебрасываем результат в массив на вывод*/
+    $next_concert                   = get_post_custom($posts[0]->ID);
+    $next_concert['wpcf-site-link'] = get_permalink($posts[0]->ID);
+
+    /*   Определяем город*/
+    if ($next_concert["wpcf-city"][0] == 1) //Если город выбран правильно, то это Мск
+    {
+        $next_concert["wpcf-city"][0] = 'г. Москва';
+    } else {
+        $next_concert["wpcf-city"][0] = $next_concert["wpcf-another_city"][0];
+    }
+
+    /*запрос на пред концерт*/
+    $posts = get_posts(
+        array(
+            'posts_per_page' => 1,
+            'post_type'      => 'concert',
+            'order'          => 'DESC',
+            'meta_key'       => 'wpcf-date',
+            'orderby'        => 'wpcf-date',
+            'meta_query'     => array(
+                array('key'     => 'wpcf-date',
+                      'value'   => time(), //Время концерта больше текущей даты
+                      'compare' => '<'
+                )
+            )
+        )
+    );
+
+    //echo '<pre>';
+    //var_dump($posts[0]);
+    $prev_concert                   = get_post_custom($posts[0]->ID);
+    //var_dump($prev_concert);
+    //echo '</pre>';
 
 
-        <table border="0" cellpadding="1" cellspacing="1" style="width: 100%;">
+    ?>
+
+    <!-- <?php echo $before_widget; ?> -->
+    <!-- <?php echo $before_title . $title . $after_title; ?> -->
+
+
+    <table border="0" cellpadding="1" cellspacing="1" style="width: 100%;">
         <tbody>
             <tr>
                 <td><h2>Ближайший концерт</h2></td>
                 <td>&nbsp;</td>
 
                     <td style="text-align: right; vertical-align: top;" title="По данным встречи в контакте" data-uk-tooltip="">
-                                    <span class="uk-badge"><dfn style="border-bottom: 0px dotted; font-style: italic;"  title="">Участники</dfn></span>&nbsp; <span class="uk-badge uk-badge-notification"><dfn style="border-bottom: 0px dotted;" title="По данным встречи в контакте">57</dfn></span>
+                                    <span class="uk-badge"><dfn style="border-bottom: 0px dotted; font-style: italic;" title="">Участники</dfn></span>&nbsp; <span class="uk-badge uk-badge-notification"><dfn style="border-bottom: 0px dotted;" title="По данным встречи в контакте">57</dfn></span>
 
                     </td>
 
@@ -31,60 +91,62 @@ function gig_main_widget($args) {
         </tbody>
         </table>
 
-        <hr style="margin-top: -3px;">
+    <hr style="margin-top: -3px;">
 
-<table border="0" cellpadding="1" cellspacing="1" style="width: 100%; margin-top: -30px;">
+    <table border="0" cellpadding="1" cellspacing="1" style="width: 100%; margin-top: -30px;">
     <tbody>
         <tr>
             <td>
-                <p class="uk-article-lead" style="font-size: 20px;">Г. Москва</p>
+                <p class="uk-article-lead" style="font-size: 20px;"><?= $next_concert["wpcf-city"][0] ?></p>
             </td>
             <td style="text-align: right;">
-                <p class="uk-article-lead" style="font-size: 20px;">20 Мая 2014г.</p>
+                <p class="uk-article-lead" style="font-size: 20px;"><?= rdate('d F Y', $next_concert["wpcf-date"][0], 1); ?></p>
             </td>
         </tr>
         <tr>
             <td>
-                <p class="uk-article-lead" style="font-size: 20px;">Клуб ТЕАТРъ</p>
+                <p class="uk-article-lead" style="font-size: 20px;"><?= $next_concert["wpcf-club"][0] ?></p>
             </td>
             <td style="text-align: right;">
-                <p class="uk-article-lead" style="font-size: 20px;">20:10</p>
+                <p class="uk-article-lead" style="font-size: 20px;"><?= date("H:i", $next_concert["wpcf-date"][0]); ?></p>
             </td>
         </tr>
         <tr>
         <br>
         </tr>
         <tr>
-            <td><a class="uk-button uk-button-primary " href="#">Подробнее</a>
+            <td><a class="uk-button uk-button-primary " href="<?= $next_concert['wpcf-site-link'] ?>" target="_blank">Подробнее</a>
             <div class="none-large"><br></div>
-             <a class="uk-button vk" href="#" target="_blank">Встреча в контакте</a>
+             <a class="uk-button vk" href="<?= $next_concert["wpcf-vk"][0]; ?>" target="_blank">Встреча в контакте</a>
 
             </td>
             <td style="text-align: right;">
-                <a href="#" target="_blank">
+                <a href="http://www.nashe.ru/" target="_blank" <? if (!$next_concert["wpcf-nradio"][0]) {
+                    echo 'style="display:none;"';
+                } ?>>
                 <div style="position: relative;float: right;zoom: 0.8;" class="nradio_recommend"></div>
                 </a>
             </td>
         </tr>
     </tbody>
 </table>
-<hr>
+    <hr>
 
-<table border="0" cellpadding="1" cellspacing="1" style="width: 100%;">
+    <table border="0" cellpadding="1" cellspacing="1" style="width: 100%;">
         <tbody>
             <tr>
                 <td><h4>Прошедший концерт</h4></td>
                 <td>
                    <blockquote>
-                        <p>Клуб FM Club</p>
-                        <small>13 Февраля</small>
+                        <p><?= $prev_concert["wpcf-club"][0] ?></p>
+                        <small><?= rdate('d F', $prev_concert["wpcf-date"][0], 1); ?></small>
                     </blockquote>
                 </td>
-
+                <? if ($prev_concert["wpcf-media"][0] == 2): ?>
                     <td style="text-align: right; width: 85px;">
-                        <a class="uk-button uk-button-primary" style="margin-bottom: 14px;" href="#">Отчет</a>
+                        <a class="uk-button uk-button-primary" style="margin-bottom: 14px;" href="<?= $prev_concert["wpcf-url_media"][0] ?>" target="_blank">Отчет</a>
                     </td>
-
+                <? endif; ?>
             </tr>
         </tbody>
         </table>
@@ -92,11 +154,12 @@ function gig_main_widget($args) {
 
 
 
-        <?php echo $after_widget; ?>
+    <?php echo $after_widget; ?>
 <?php
 }
 
-function gig_main_widget_control() {
+function gig_main_widget_control()
+{
     /*$options = $newoptions = get_option('a2p_plugin_widget_meta');
     if ( $_POST["a2p_plugin_meta-submit"] ) {
         $newoptions['title'] = strip_tags(stripslashes($_POST["a2p_plugin_meta-title"]));
@@ -112,7 +175,8 @@ function gig_main_widget_control() {
 <?php*/
 }
 
-function gig_main_widget_init() {
+function gig_main_widget_init()
+{
     register_sidebar_widget('gig_main', 'gig_main_widget');
     register_widget_control('gig_main', 'gig_main_widget_control', 300, 90);
 }
