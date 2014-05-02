146e2e8dd36f7d50c3624b7f407e4c941dd10688
Руслан <kraktus@mail.ru>
2014-05-02 11:27:40 +0400

    Плагин "ссылки в футере" Проброшенны линки

--- wp-content/plugins/footer_links/footer_links.php
+++ wp-content/plugins/footer_links/footer_links.php
@@ -29,73 +29,113 @@ function footer_links_widget($args) {
         <tbody>
                 <tr style="height:70px">
                         <td width="90px">
+                            <a href="http://www.nashe.ru/" target="_blank">
                                 <img width="90px" src="/wp-content/plugins/footer_links/img/1.png">
+                            </a>
                         </td>
                         <td width="110px">
+                            <a href="http://www.nashe.ru/nashe20/" target="_blank">
                                 <img width="70px" style="margin-left: 10px;" src="/wp-content/plugins/footer_links/img/2.png">
+                            </a>
                         </td>
                         <td width="110px">
+                            <a href="http://nashestvie.ru/" target="_blank">
                                 <img width="110px" style="margin-left: -10px;" src="/wp-content/plugins/footer_links/img/3.png">
+                            </a>
                         </td>
                         <td width="110px">
+                            <a href="http://vozduh.info/" target="_blank">
                                 <img width="110px" style="margin-left: 7px;" src="/wp-content/plugins/footer_links/img/4.png">
+                            </a>
                         </td>
                 </tr>
                 <tr style="height:60px">
                         <td>
+                               <a href="http://vk.com/semenchayka" target="_blank">
                                 <img width="90px" src="/wp-content/plugins/footer_links/img/5.png">
+                               </a>
                         </td>
                         <td>
+                            <a href="http://www.musicmoscow.ru" target="_blank">
                                 <img width="90px" src="/wp-content/plugins/footer_links/img/6.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://www.moskva.fm/" target="_blank">
                                 <img width="100px" src="/wp-content/plugins/footer_links/img/7.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://www.korol-i-shut.ru/" target="_blank">
                                 <img width="110px" style="margin-left: 10px;" src="/wp-content/plugins/footer_links/img/8.png">
+                            </a>
                         </td>
                 </tr>
                 <tr>
                         <td width="80px">
+                            <a href="http://vk.com/cwa_afisha" target="_blank">
                                 <img width="80px" style="margin-left: 10px;" src="/wp-content/plugins/footer_links/img/9.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://vk.com/cube_music" target="_blank">
                                 <img width="50px" style="margin-left: 21px;" src="/wp-content/plugins/footer_links/img/10.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://vk.com/live_night_music" target="_blank">
                                 <img width="53px" style="margin-left: 20px;" src="/wp-content/plugins/footer_links/img/11.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://vk.com/yanakarataeva" target="_blank">
                                 <img width="110px" style="margin-left: 10px; margin-bottom: 10px" src="/wp-content/plugins/footer_links/img/12.png">
+                            </a>
                         </td>
                 </tr>
                 <tr style="height:65px">
                         <td>
+                            <a href="http://vk.com/club42097095" target="_blank">
                                 <img width="90px" style="margin-left: 6px;" src="/wp-content/plugins/footer_links/img/13.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://www.art-seg.ru/" target="_blank">
                                 <img width="70px" style="margin-left: 12px;" src="/wp-content/plugins/footer_links/img/14.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://q-rec.ru/" target="_blank">
                                 <img width="73px" style="margin-left: 13px;" src="/wp-content/plugins/footer_links/img/15.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://rock-online.ru/" target="_blank">
                                 <img width="55px" style="margin-left: 30px;" src="/wp-content/plugins/footer_links/img/16.png">
+                            </a>
                         </td>
                 </tr>
 
                 <tr style="height:60px">
                         <td>
+                            <a href="http://teatrclub.com/" target="_blank">
                                 <img width="80px" style="margin-left: 13px;" src="/wp-content/plugins/footer_links/img/17.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://rusrock.info/" target="_blank">
                                 <img width="45px" style="margin-left: 26px;" src="/wp-content/plugins/footer_links/img/18.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://rocktusa.ru/" target="_blank">
                                 <img width="50px" style="margin-left: 25px; margin-bottom: 10px" src="/wp-content/plugins/footer_links/img/19.png">
+                            </a>
                         </td>
                         <td>
+                            <a href="http://www.glastonberrypub.ru/" target="_blank">
                                 <img width="110px" style="margin-left: 15px;" src="/wp-content/plugins/footer_links/img/20.png">
+                            </a>
                         </td>
                 </tr>
         </tbody>
